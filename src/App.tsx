0589cb14d586f52f435559ee5a9b6c5870aa0fd8
Rickcel Bantog <rb@Rickcels-MacBook-Pro.local>
2023-10-08 02:54:40 -0700

    functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { ReactElement } from "react";
-import Form from "./components/Form";
-import Grid from "./components/Grid";
+import { ReactElement } from 'react';
+import Form from './components/Form';
+import Grid from './components/Grid';
 
 function App(): ReactElement {
-  return (
-    <>
-      <Form />
-      <Grid />
-    </>
-  );
+    return (
+        <div className="m-4">
+            <p className="text-center m-4">Generate your Pokémon team</p>
+            <Form />
+            <Grid />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,34 +1,63 @@
-import { ReactElement } from "react";
-import { useSearchParams } from "react-router-dom";
-import { sanitizeString } from "../helper/sanitize";
+import { ReactElement } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import seedrandom from 'seedrandom';
+
+import { getPokemonImg, pokemonData, PokemonData } from '../data/pokemon';
+import { sanitizeString } from '../helper/sanitize';
+import pokeball from '../images/pokeball-sprite.png';
+
+const MIN: number = 1;
+const MAX: number = 1017;
+
+function getPokemon(name: string): ReactElement[] {
+    const pokemon: ReactElement[] = [];
+
+    if (!name) {
+        //default
+        for (let i = 0; i < 6; i++) {
+            pokemon.push(
+                <div className="text-lg font-bold text-center p-10 rounded-lg border border-black">
+                    <img src={pokeball} />
+                </div>
+            );
+        }
+    } else {
+        let rng;
+        let pokemonNumber: string;
+        let pokemonName: string;
+
+        for (let i = 0; i < 6; i++) {
+            rng = seedrandom(`${name}--${i}`);
+            pokemonNumber = String(Math.floor(rng() * (MAX - MIN + 1) + MIN));
+            pokemonName = (pokemonData as PokemonData)[pokemonNumber].name;
+            pokemon.push(
+                <div className="text-lg font-bold text-center items-center p-10 rounded-lg border border-black">
+                    <img
+                        className="inline-block min-w-full"
+                        src={getPokemonImg(
+                            pokemonName,
+                            parseInt(pokemonNumber)
+                        )}
+                    />
+                    <p>{pokemonName}</p>
+                </div>
+            );
+        }
+    }
+
+    return pokemon;
+}
 
 function Grid(): ReactElement {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const name = sanitizeString(searchParams.get("name"));
+    const [searchParams, _] = useSearchParams();
+    const name = sanitizeString(searchParams.get('name'));
 
-  return (
-    <div className="flex items-center justify-center">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 p-5">
-        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
-          1
+    return (
+        <div className="flex items-center justify-center">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 p-5">
+                {getPokemon(name)}
+            </div>
         </div>
-        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
-          2
-        </div>
-        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
-          3
-        </div>
-        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
-          4
-        </div>
-        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
-          5
-        </div>
-        <div className="bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">
-          6
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 export default Grid;
